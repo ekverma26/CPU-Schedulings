2224d1a666400be8b1b8730f029c8819a3041096
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2019-09-27 23:20:00 +0530

    Add files via upload

---
+++ ljf.java
@@ -0,0 +1,65 @@
+public class ljf
+{
+static void calWaitTime(int process[],int n,int bt[],int wt[])
+{	
+	int i;
+	wt[0]=0;
+	for(i=1;i<n;i++)
+	{wt[i]=bt[i-1]+wt[i-1];   }
+}
+static void calTurnTime(int process[],int n,int bt[],int wt[],int tat[])
+{
+	int i;
+	for(i=0;i<n;i++)
+	{  tat[i]=bt[i]+wt[i];  }
+}
+static void calAvgTime(int process[],int n,int bt[])
+{
+	int wt[]=new int[n];
+	int tat[]=new int[n];
+	int twt=0,ttat=0;
+	int i,j,temp;
+	for(i=0;i<3;i++)
+	{
+	for(j=i+1;j<3;j++)
+	{
+	if (bt[i]<bt[j])
+	{
+	temp=bt[i];
+	bt[i]=bt[j];
+	bt[j]=temp;
+	temp=process[i];
+	process[i]=process[j];
+	process[j]=temp;
+	}	}	}
+	calWaitTime(process,n,bt,wt);
+	calTurnTime(process,n,bt,wt,tat);
+	for(i=0;i<n;i++)
+	{
+	twt=twt+wt[i];
+	ttat=ttat+tat[i];
+	}
+	int w,t;
+	w=twt/n;
+	t=ttat/n;	
+	System.out.printf("Processes  Burst_Time  Waiting_Time  Turn_around_time\n");
+	for(i=0;i<n;i++)
+	{
+	System.out.printf("%d\t\t",process[i]);
+	System.out.printf("%d\t\t",bt[i]);
+	System.out.printf("%d\t\t",wt[i]);
+	System.out.printf("%d\n",tat[i]);
+	}
+	System.out.printf("Average Waiting Time:%d",w);
+	System.out.printf("\n");
+	System.out.printf("Average Turn Around Time:%d",t);
+}
+
+public static void main(String args[])
+{
+int process[]={1,2,3};
+int bt[]={7,1,14};
+int n=process.length;
+calAvgTime(process,n,bt);
+}
+}
